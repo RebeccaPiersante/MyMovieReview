82201b5c60ca6a347c954a3bf91c25a9f59ec1e4
RebeccaPiersante <rebpiers@gmail.com>
2026-04-17 16:46:37 +0200

    Initial commit

---
+++ src/main/java/com/rebecca/mymoviereview/MymoviereviewApplication.java
@@ -0,0 +1,13 @@
+package com.rebecca.mymoviereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymoviereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymoviereviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebecca/mymoviereview/MymoviereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebecca.mymoviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymoviereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
